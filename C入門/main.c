30e587d4a50bf4bc4b7f30208fcd050511c420da
犬塚眞太郎 <studio_graph@inudzukashintarou-no-MacBook-Air.local>
2013-06-29 16:34:52 +0900

    Initial Commit

---
+++ C入門/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C入門
+//
+//  Created by 犬塚眞太郎 on 2013/06/29.
+//  Copyright (c) 2013年 犬塚眞太郎. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
